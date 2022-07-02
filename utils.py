60b453ee2c6f89a1b3f0903cabbe795e6a452920
Mark Schultz <mdschultz@eng.ucsd.edu>
2022-07-02 23:01:27 +0200

    More utilities

--- utils.py
+++ utils.py
@@ -11,8 +11,16 @@ def sample_bounded_vec(n, B):
 def sample_unif_matrix(n, q):
     return [sample_unif_vec(n,q) for _ in range(n)]
 
+def sample_bounded_matrix(n, B):
+    return [sample_bounded_vec(n,B) for _ in range(n)]
+
 def matrix_vector_multiply(m, v):
+    n = len(m)
     return [sum(m[i][j] * v[j] for j in range(n)) for i in range(n)]
+
+def matrix_matrix_multiply(m, v):
+    n = len(m)
+    return [[sum(m[i][j] * v[j][k] for j in range(n)) for i in range(n)] for k in range(n)]
 
 def matrix_transpose(m):
     n = len(m)
