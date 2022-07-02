dc46cd7a4006a74b807b3cdb5d1d4e1cb9e02131
Mark Schultz <mdschultz@eng.ucsd.edu>
2022-07-02 23:47:00 +0200

    priv key done

--- private_key.py
+++ private_key.py
@@ -35,27 +35,29 @@ class LWEPrivKey:
         self.s = sample_unif_vector(self.n, self.q)
 
     def enc(self, m):
-        # Sampling the various values
+        # Sampling A, e
         A = sample_unif_matrix(n, self.q)
         e = sample_bounded_vector(self.n, self.B)
-        # Computing b := As
-        b = matrix_vector_multiply(A, self.s)
-        b = self.LWE_func(A, self.s, e)
-        # Adding a scaled copy of m to b
-        for i in range(self.n):
-            b[i] += m[i] * (q//2) % self.q
+        # Computing b := As + e
+        b = matrix_vector_multiply(A, self.s, self.q)
+        b = vector_vector_add(b, e, self.q)
+        # Scaling m -> (q//2) m
+        scaled_m = [(self.q//2) * m[i] % self.q for i in range(self.n)]
+        # Adding (q//2)m to b = As + e
+        b = vector_vector_add(b, scaled_m, self.q)
         return (A, b)
 
     def dec(self, ctxt):
         (A, b) = ctxt[0], ctxt[1]
-        # Recomputing As and subtracting it from b:= As + (q/2)m + e to get (q/2)m + e
+        # Recomputing As
+        As = matrix_vector_multiply(A, self.s, self.q)
+        # Recovering scaled_m = b - As
         for i in range(self.n):
-            for k in range(self.n):
-                b[i] -= A[i][k] * self.s[k] % self.q
-        # Recovering m from (q/2)m + e
+            b[i] = (b[i] - As[i]) % q
+        # Scaling (q//2)m + e -> m
         m = [0 for _ in range(self.n)]
         for i in range(self.n):
-            m[i] = round(b[i] / (q//2)) % 2
+            m[i] = round(b[i] / (self.q//2)) % 2
         return m
 
 if __name__ == "__main__":
@@ -63,14 +65,14 @@ if __name__ == "__main__":
     q = 3000
     n = 1000
     B = 10
-    E = NoiselessPrivKey(n,q)
+    # E = NoiselessPrivKey(n,q)
+    E = LWEPrivKey(n,q, B)
     t= time.time()
     E.key_gen()
     t_keys = time.time()
     print(f"Generating keys took {t_keys-t} sec")
     m = [random.randint(0,1) for _ in range(n)]
     print("Encrypting Random message:")
-    print(m)
     t = time.time()
     c =E.enc(m)
     t_enc =time.time()
